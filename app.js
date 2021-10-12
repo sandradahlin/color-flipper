075e44a2356e6f21292176e65d9b5c1146cbdf99
Aleksandra Dahlin <Aleksandra.Dahlin@ikano.se>
2021-10-12 19:32:10 +0200

    get random color

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNr = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNr];
+  color.innerHTML = colors[randomNr];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
--- hex.js
+++ hex.js
@@ -1,1 +1,19 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+//Get random color
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    let randomChar = getRandomChar();
+    hexColor += hex[randomChar];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomChar() {
+  return Math.floor(Math.random() * hex.length);
+}
